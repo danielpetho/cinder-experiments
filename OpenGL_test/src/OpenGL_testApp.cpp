87d90eecaff0cf0f6f0b4907138a41f0d7734194
emzeperix <kpethodaniel@gmail.com>
2021-02-19 20:51:58 +0100

    Fixed off-screen buffer rendering to screen

--- OpenGL_test/src/OpenGL_testApp.cpp
+++ OpenGL_test/src/OpenGL_testApp.cpp
@@ -3,8 +3,12 @@
 #include "cinder/gl/gl.h"
 #include "cinder/Utilities.h"
 #include "cinder/Easing.h"
+#include "cinder/gl/Fbo.h"
+#include "cinder/ImageIo.h"
+
 
 #include "Windows.h"
+
 
 
 using namespace ci;
@@ -23,42 +27,60 @@ public:
 	CameraPersp		mCam;
 	gl::BatchRef	mSlices[NUM_SLICES];
 
-	int				mCurrentFrame;
+	unsigned int    mCurrentFrame;
+
+	gl::FboRef myFbo = gl::Fbo::create(1440, 2160);
 };
 
 void OpenGL_testApp::setup()
 {
+
+	myFbo->bindFramebuffer();
 	auto lambert = gl::ShaderDef().lambert().color();
 	gl::GlslProgRef	shader = gl::getStockShader(lambert);
+	
+	
 
 	for (int i = 0; i < NUM_SLICES; ++i) {
 		float rel = i / (float)NUM_SLICES;
 		float sliceHeight = 1.0f / NUM_SLICES;
-		auto slice = geom::Cube().size(1, sliceHeight, 1);
-		auto trans = geom::Translate(0, rel * 1.5f, 0);
+		auto slice = geom::Cube().size(2, sliceHeight, 2);
+		auto trans = geom::Translate(0, rel * 2.0f, 0);
 		auto color = geom::Constant(geom::COLOR,
-			Color(CM_HSV, 266.0f / 360.0f, 1, rel));
+			Color(CM_HSV, 266.0f / 360.0f, 1, rel * 1.0f));
 		mSlices[i] = gl::Batch::create(slice >> trans >> color,
 			shader);
 	}
 
+	mCurrentFrame = 0;
+	
+	
 
-	mCam.lookAt(vec3(2, 2, 3), vec3(0, 0.5f, 0));
 }
 
 void OpenGL_testApp::mouseDown(MouseEvent event)
 {
 	// pull down the current window as a surface and pass it to writeImage
-	writeImage(getHomeDirectory() / "cinder" / "saveImage_" / (toString(mCurrentFrame) + ".png"), copyWindowSurface());
+	writeImage(getHomeDirectory() / "cinder" / "saveImage_" / (toString(mCurrentFrame) + ".png"), myFbo->getColorTexture()->createSource());
 	mCurrentFrame++;
 }
 
 void OpenGL_testApp::draw()
 {
+	
+	
 	gl::clear();
+
+	mCam.lookAt(vec3(3, 3, 5), vec3(0, 0.65, 0));
+
+	//gl::ScopedMatrices matrices;
+	//gl::setMatricesWindowPersp(myFbo->getSize(), 60.0f, 1.0f, 1000.0f, true);
+	gl::ScopedViewport viewport(vec2(0), myFbo->getSize());
+	
+
 	gl::enableDepthRead();
 	gl::enableDepthWrite();
-
+	
 	gl::setMatrices(mCam);
 
 	const float delay = 0.1f;
@@ -73,6 +95,7 @@ void OpenGL_testApp::draw()
 	// loop every 'totalTime' seconds
 	float time = fmod(getElapsedFrames() / 30.0f, totalTime);
 
+	
 	for (int i = 0; i < NUM_SLICES; ++i) {
 		// animates from 0->1
 		float rotation = 0;
@@ -88,11 +111,20 @@ void OpenGL_testApp::draw()
 
 		gl::ScopedModelMatrix scpModelMtx;
 		gl::rotate(angleAxis(angle, vec3(0, 1, 0)));
+		
 		mSlices[i]->draw();
+
 	}
+
+	//writeImage(getHomeDirectory() / "cinder" / "saveImage_" / (toString(mCurrentFrame) + ".png"), myFbo->getColorTexture()->createSource());
+	//gl::Texture2dRef screenTexture = myFbo->getColorTexture();
+	//writeImage(getHomeDirectory() / "cinder" / "saveImage_" / (toString(mCurrentFrame) + ".png"), screenTexture->createSource());
+
+	myFbo->blitToScreen(myFbo->getBounds(), getWindowBounds());
+
+	mCurrentFrame++;
 }
 
 CINDER_APP( OpenGL_testApp, RendererGl, [](App::Settings *settings) {
-	settings->setWindowSize(700, 700);
-	settings->setMultiTouchEnabled(false);
+	settings->setWindowSize(1440 / 3, 2160 / 3);
 })
